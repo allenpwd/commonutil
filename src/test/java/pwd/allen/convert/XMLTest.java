b5efd17e10ea7a3ad63dd4236f62e02f40c83d9b
pwdan <994266136@qq.com>
2020-11-03 13:21:57 +0800

    SAXBuilder读取xml

--- src/test/java/pwd/allen/convert/XMLTest.java
+++ src/test/java/pwd/allen/convert/XMLTest.java
@@ -1,8 +1,13 @@
 package pwd.allen.convert;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -35,5 +40,28 @@ public class XMLTest {
         System.out.println(c.getTime().getHours());
 
         System.out.println(new SimpleDateFormat("yyyyMMdd0SSS").format(new Date()));
+    }
+
+    /**
+     * java saxbuilder 读取xml
+     * @throws JDOMException
+     * @throws IOException
+     */
+    @Test
+    public void test2() throws JDOMException, IOException {
+        SAXBuilder sb = new SAXBuilder();
+
+        Document doc = sb.build(XMLTest.class.getClassLoader().getResourceAsStream("xml/test.xml")); //构造文档对象
+        Element root = doc.getRootElement(); //获取根元素HD
+        List list = root.getChildren("bean");//取名字为disk的所有元素
+        for (int i = 0; i < list.size(); i++) {
+            Element element = (Element) list.get(i);
+            String id = element.getAttributeValue("id");
+            String clazz = element.getAttributeValue("class");
+            String name = element.getChildText("name");//取disk子元素capacity的内容
+            System.out.println(id);
+            System.out.println(clazz);
+            System.out.println(name);
+        }
     }
 }
